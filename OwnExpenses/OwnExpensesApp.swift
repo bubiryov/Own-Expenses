2226fefe37565b24c9f7d5b0773557e51469a6f0
Egor <bubiryov@icloud.com>
2023-02-01 12:40:25 +0200

    Initial Commit

---
+++ OwnExpenses/OwnExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OwnExpensesApp.swift
+//  OwnExpenses
+//
+//  Created by Egor Bubiryov on 01.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct OwnExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
